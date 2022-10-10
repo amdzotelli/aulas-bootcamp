969c70bdf2aa49a0cdcf29ab610c565f72d316e6
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-10-10 15:57:32 -0300

    aula 09 - testing 01 - pt 2

---
+++ aula09/teste02/src/main/java/com/teste02/teste02/exception/ExceptionDetails.java
@@ -0,0 +1,17 @@
+package com.teste02.teste02.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class ExceptionDetails {
+    private String title;
+    private String message;
+    private String fields;
+    private String fieldsMessages;
+    private LocalDateTime timestamp;
+
+}
